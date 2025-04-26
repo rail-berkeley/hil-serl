6557a2525108bf267b51710731bef76a46ca7911
Rico Qi <qirico@outlook.com>
2025-04-26 11:15:10 -0700

    feat: Added script to inspect success/failure data.

--- examples/inspect_data.py
+++ examples/inspect_data.py
@@ -10,6 +10,29 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string("folder", None, "Path to folder (either /buffer/, /demo_buffer/, /preference_buffer/, or /interventions/)")
 
 
+def _save_video(frames: np.ndarray, default_path: str):
+    print()
+    print("Output path: ")
+    output_path = input(f"[{default_path}] ")
+    if output_path == "":
+        output_path = f"{default_path}"
+    output_path = os.path.abspath(output_path)
+    print(output_path)
+    assert os.path.exists(os.path.dirname(output_path))
+    if os.path.exists(output_path):
+        print()
+        yn = input("Will override previous video. Proceed? [y/n] [n] ")
+        if yn != 'y':
+            return
+    print()
+    imageio.mimsave(
+        uri=output_path,
+        ims=frames,
+        fps=10,
+        macro_block_size=None  # This can help with sizes not multiples of 16
+    )
+
+
 def inspect_buffer(path: str):
     files = [os.path.join(path, fp) for fp in os.listdir(path) if os.path.isfile(os.path.join(path, fp))]
     transitions = []
@@ -84,26 +107,69 @@ def inspect_buffer(path: str):
         frames.append(new_frame)
     frames = np.stack(frames, axis=0)
 
+    _save_video(frames, "./video.mp4")
+
+
+def inspect_classifier_data(path: str):
+    assert os.path.exists(path), path
+    files = [os.path.join(path, fp) for fp in os.listdir(path) if os.path.isfile(os.path.join(path, fp))]
+    successes = []
+    failures = []
+    for fp in files:
+        assert os.path.basename(fp).endswith(".pkl")
+        if not os.path.basename(fp).endswith(".pkl"):
+            continue
+        with open(fp, "rb") as f:
+            obj = pickle.load(f)
+        assert isinstance(obj, list)
+        if len(obj) == 0:
+            continue
+        assert isinstance(obj[0], dict)
+        assert set(obj[0].keys()).issuperset(['observations', 'actions', 'next_observations', 'rewards', 'masks', 'dones'])
+        if "success" in os.path.basename(fp):
+            successes += obj
+        elif "failure" in os.path.basename(fp):
+            failures += obj
+        else:
+            raise Exception()
+    
+    camera_keys = list(successes[0]["observations"].keys() - {"state"})
+    assert len(camera_keys) > 0
     print()
-    print("Output path: ")
-    output_path = input("[./video.mp4] ")
-    if output_path == "":
-        output_path = "./video.mp4"
-    output_path = os.path.abspath(output_path)
-    print(output_path)
-    assert os.path.exists(os.path.dirname(output_path))
-    if os.path.exists(output_path):
-        print()
-        yn = input("Will override previous video. Proceed? [y/n] [n] ")
-        if yn != 'y':
-            return
-    print()
-    imageio.mimsave(
-        uri=output_path,
-        ims=frames,
-        fps=10,
-        macro_block_size=None  # This can help with sizes not multiples of 16
-    )
+    print("Pick a camera from the following cameras:")
+    print(camera_keys)
+    camera_key = input(f"['{camera_keys[0]}'] ")
+    if camera_key == "":
+        camera_key = camera_keys[0]
+    assert camera_key in camera_keys
+    print(camera_key)
+
+    success_frames = []
+    for i, tr in enumerate(successes):
+        frame = tr["observations"][camera_key]
+        assert isinstance(frame, np.ndarray)
+        assert frame.dtype == np.uint8
+        assert frame.shape == (1, 128, 128, 3)
+        frame = frame[0]
+        new_frame = np.zeros((450, 400, 3), dtype=np.uint8)
+        new_frame[50:,:,:] = cv2.resize(frame, (400, 400))
+        success_frames.append(new_frame)
+    success_frames = np.stack(success_frames, axis=0)
+
+    failure_frames = []
+    for i, tr in enumerate(failures):
+        frame = tr["observations"][camera_key]
+        assert isinstance(frame, np.ndarray)
+        assert frame.dtype == np.uint8
+        assert frame.shape == (1, 128, 128, 3)
+        frame = frame[0]
+        new_frame = np.zeros((450, 400, 3), dtype=np.uint8)
+        new_frame[50:,:,:] = cv2.resize(frame, (400, 400))
+        failure_frames.append(new_frame)
+    failure_frames = np.stack(failure_frames, axis=0)
+
+    _save_video(success_frames, "./successes.mp4")
+    _save_video(failure_frames, "./failures.mp4")
 
 
 def main(_):
@@ -111,6 +177,8 @@ def main(_):
     folder_name = os.path.basename(path)
     if folder_name == "buffer":
         inspect_buffer(path)
+    elif folder_name == "classifier_data":
+        inspect_classifier_data(path)
     else:
         raise Exception()
 
